f81ec72a8f5fa7abc9574f46ed5d025beda0a018
William Trevisan <williamtrev@hotmail.com>
2022-03-27 14:18:24 -0300

    Created controller for Team and method for create a new team, also created resource for controllers methods returns and test for store method in team controller

---
+++ tests/Unit/App/Http/Controllers/Api/TeamControllerUnitTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit\App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\TeamController;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Models\Team as TeamModel;
+use App\Repositories\Eloquent\TeamEloquentRepository;
+use Core\Domain\Repositories\TeamRepositoryInterface;
+use Core\UseCases\Team\CreateTeamUseCase;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+class TeamControllerUnitTest extends TestCase
+{
+    protected TeamRepositoryInterface $teamEloquentRepository;
+    protected Controller $teamController;
+
+    protected function setUp(): void
+    {
+        $this->teamEloquentRepository = new TeamEloquentRepository(new TeamModel());
+        $this->teamController = new TeamController();
+
+        parent::setUp();
+    }
+
+    public function testShouldBeAbleToCreateANewTeam()
+    {
+        $createTeamUseCase = new CreateTeamUseCase($this->teamEloquentRepository);
+        $request = new StoreCategoryRequest();
+        $request->headers->set('Content-type', 'application/json');
+        $request->setJson(new ParameterBag(['description' => 'Internacional']));
+
+        $response = $this->teamController->store($request, $createTeamUseCase);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_CREATED, $response->status());
+    }
+}
