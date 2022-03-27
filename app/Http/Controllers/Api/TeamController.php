f37f95948e6e45fa6fdbcf72b22bff6b02f29854
William Trevisan <williamtrev@hotmail.com>
2022-03-27 14:27:41 -0300

    Adjusted TeamController path, and created api resources routes from teams

---
+++ app/Http/Controllers/Api/TeamController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Resources\TeamResource;
+use Core\UseCases\Team\CreateTeamUseCase;
+use Core\UseCases\Team\dtos\CreateTeamInputDTO;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+class TeamController extends Controller
+{
+    public function store(StoreCategoryRequest $request, CreateTeamUseCase $createTeamUseCase): JsonResponse
+    {
+        $response = $createTeamUseCase->execute(
+            input: new CreateTeamInputDTO(
+                description: $request->description,
+                isActive: (bool) $request->is_active ?? true,
+            )
+        );
+
+        return (new TeamResource(collect($response)))
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);
+    }
+}
